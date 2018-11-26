031373b78ea8ad707c39576c04360ebe0954db98
luischomaacad <38477331+luischomaacad@users.noreply.github.com>
2018-11-26 11:44:18 -0200

    internal docs

--- trab-yama-api/db.js
+++ trab-yama-api/db.js
@@ -1,5 +1,10 @@
 const mysql = require("mysql");
 
+/*
+O código abaixo cria uma conexão com o banco mysql
+no schema 'yamagod', tendo como host o próprio localhost.
+*/
+
 var con = mysql.createConnection({
   host: "127.0.0.1",
   user: "yama",
@@ -7,8 +12,8 @@ var con = mysql.createConnection({
   database: "yamagod"
 });
 
-con.connect(function(err) {
+con.connect(function(err) { // Tratamento de erro
   if (err) throw err;
 });
 
-module.exports = con;
+module.exports = con; // Expondo a conexão
