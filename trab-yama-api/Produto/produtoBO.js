e15830aee81a2277318ea287a20d4199cf18d629
luischomaacad <38477331+luischomaacad@users.noreply.github.com>
2018-11-26 11:57:57 -0200

    Update produtoBO.js

--- trab-yama-api/Produto/produtoBO.js
+++ trab-yama-api/Produto/produtoBO.js
@@ -1,5 +1,12 @@
 const produtoDao = require("./produtoDAO");
 
+/*
+Seguindo o padrão de arquitetura, a produtoBO é chamada
+com um tipo de requisição e, dependendo do tipo, chama
+os métodos presentes na classe DAO com o respectivo dado
+e.g: ao receber um delete -> chama a produtoDAO com o código
+*/
+
 produtoBO = {
   insert: produto => {
     return produtoDao.insert(produto);
